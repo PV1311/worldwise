872e8069b41a458ad4ee3a5015552e4ac3ffe118
Pranjal Verma <127687791+PV1311@users.noreply.github.com>
2025-05-15 18:46:01 +0530

    Update Logo.jsx

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -7,7 +7,6 @@ function Logo() {
       <img src="/logo.png" alt="WorldWise logo" className={styles.logo} />
     </Link>
   );
-  // on clicking on the Logo we should return to homepage so we wrapped the <img> element in <Link/> component. This time we are using <Link/> because we don't want to add any special styles when this is selected
 }
 
 export default Logo;
