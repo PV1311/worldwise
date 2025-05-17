85b6235ec93992bf061c3b2af68b3fcaa6f446d8
Pranjal Verma <127687791+PV1311@users.noreply.github.com>
2025-05-17 17:09:03 +0530

    Update CitiesContext.jsx

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -1,13 +1,15 @@
 import {
   createContext,
-  // useState,
   useEffect,
   useContext,
   useReducer,
   useCallback,
 } from "react";
 
-const BASE_URL = "http://localhost:9000";
+// const BASE_URL = "http://localhost:9000";
+
+const BASE_URL =
+  "https://worldwise-b8f2n-default-rtdb.europe-west1.firebasedatabase.app";
 
 const CitiesContext = createContext();
 
@@ -15,8 +17,7 @@ const iniitalState = {
   cities: [],
   isLoading: false,
   currentCity: {},
-  error: "", // we also created an error state for which we created case 'rejected' in reducer. We are not going to use this state in our application beacause we are not
-  //            doing much error handling, but this was just to make it a bit more complete
+  error: "", 
 };
 
 function reducer(state, action) {
@@ -24,9 +25,6 @@ function reducer(state, action) {
     case "loading":
       return { ...state, isLoading: true };
     case "cities/loaded":
-      // it is usually a good idea to use a meaningful naming convention when naming action types. So it is a good idea to model the actions as events and not as setters.
-      // For example, it should not be cities, but instead we can call it cities/loaded. WE could also use citiesLoaded but it is a bit of a naming convention to use a
-      // slash like cities/loaded, atleast in the Redux xommunity which is actually similar to what we are implementing here
       return { ...state, isLoading: false, cities: action.payload };
     case "city/loaded":
       return { ...state, isLoading: false, currentCity: action.payload };
@@ -52,41 +50,23 @@ function reducer(state, action) {
 }
 
 function CitiesProvider({ children }) {
-  // const [cities, setCities] = useState([]);
-  // const [isLoading, setIsLoading] = useState(false);
-  // const [currentCity, setCurrentCity] = useState(false);
-
   const [{ cities, isLoading, currentCity, error }, dispatch] = useReducer(
-    // we immediately destructure the state back here into cities, isLoading, currentCity and error(which we are not using but we passed it into the context as well)
     reducer,
     iniitalState
   );
 
-  // useEffect(function () {
-  //   async function fetchCities() {
-  //     try {
-  //       setIsLoading(true);
-  //       const res = await fetch(`${BASE_URL}/cities`);
-  //       const data = await res.json();
-  //       setCities(data);
-  //     } catch {
-  //       alert("There was an error loading data");
-  //     } finally {
-  //       setIsLoading(false);
-  //     }
-  //   }
-  //   fetchCities();
-  // }, []);
-
+  // UPDATED FOR FIREBASE:
   useEffect(function () {
     async function fetchCities() {
       dispatch({ type: "loading" });
 
       try {
-        const res = await fetch(`${BASE_URL}/cities`);
+        const res = await fetch(`${BASE_URL}/cities.json`);
         const data = await res.json();
 
-        dispatch({ type: "cities/loaded", payload: data });
+        const citiesArray = data ? Object.values(data) : [];
+
+        dispatch({ type: "cities/loaded", payload: citiesArray });
       } catch {
         dispatch({
           type: "rejected",
@@ -97,52 +77,19 @@ function CitiesProvider({ children }) {
     fetchCities();
   }, []);
 
-  // async function getCity(id) {
-  //   try {
-  //     setIsLoading(true);
-  //     const res = await fetch(`${BASE_URL}/cities/${id}`);
-  //     const data = await res.json();
-  //     setCurrentCity(data);
-  //   } catch {
-  //     alert("There was an error loading data");
-  //   } finally {
-  //     setIsLoading(false);
-  //   }
-  // }
-
-  // async function getCity(id) {
-  //   if (Number(id) === currentCity.id) return; // here we check if the id that is being passed in is the same as the currentCity and so we can check is the city that
-  //   //                                            we want to load is already the current city and so if it is, there is no need to call the API again. Here id is
-  //   //                                            converted to Number because it is coming from URL so it is a string
-
-  //   dispatch({ type: "loading" });
-
-  //   try {
-  //     const res = await fetch(`${BASE_URL}/cities/${id}`);
-  //     const data = await res.json();
-
-  //     dispatch({ type: "city/loaded", payload: data });
-  //   } catch {
-  //     dispatch({
-  //       type: "rejected",
-  //       payload: "There was an error loading the city...",
-  //     });
-  //   }
-  // }
-
+  // UPDATED FOR FIREBASE:
   const getCity = useCallback(
     async function getCity(id) {
-      if (Number(id) === currentCity.id) return; // here we check if the id that is being passed in is the same as the currentCity and so we can check is the city that we
-      //                                            want to load is already the current city and so if it is, there is no need to call the API again. Here id is converted to
-      //                                            Number because it is coming from URL so it is a string
-
+      if (id === currentCity.id) return;
       dispatch({ type: "loading" });
 
       try {
-        const res = await fetch(`${BASE_URL}/cities/${id}`);
+        const res = await fetch(`${BASE_URL}/cities.json`);
         const data = await res.json();
+        const citiesArray = Object.values(data);
+        const city = citiesArray.find((city) => city.id === id);
 
-        dispatch({ type: "city/loaded", payload: data });
+        dispatch({ type: "city/loaded", payload: city });
       } catch {
         dispatch({
           type: "rejected",
@@ -153,44 +100,24 @@ function CitiesProvider({ children }) {
     [currentCity.id]
   );
 
-  // async function createCity(newCity) {
-  //   try {
-  //     setIsLoading(true);
-  //     const res = await fetch(`${BASE_URL}/cities`, {
-  //       method: "POST",
-  //       body: JSON.stringify(newCity),
-  //       headers: {
-  //         "content-type": "application/json", // here the content-type header is set to application/json, just so the API knows, what data format it is receiving
-  //       },
-  //     });
-  //     const data = await res.json();
 
-  //     // console.log(data);
-
-  //     setCities((cities) => [...cities, data]);
-  //   } catch {
-  //     alert("There was an error creating city");
-  //   } finally {
-  //     setIsLoading(false);
-  //   }
-  // }
-
+  //UPDATED FOR FIREBASE:
   async function createCity(newCity) {
     dispatch({ type: "loading" });
-
     try {
-      const res = await fetch(`${BASE_URL}/cities`, {
+      const res = await fetch(`${BASE_URL}/cities.json`, {
         method: "POST",
         body: JSON.stringify(newCity),
         headers: {
-          "content-type": "application/json", // here the content-type header is set to application/json, just so the API knows, what data format it is receiving
+          "Content-Type": "application/json",
         },
       });
       const data = await res.json();
 
-      // console.log(data);
+      // Firebase returns { name: "-uniqueKey" }, we need to add that
+      const createdCity = { ...newCity, firebaseId: data.name };
 
-      dispatch({ type: "city/created", payload: data }); // here we keep our remote state in sync with our UI state
+      dispatch({ type: "city/created", payload: createdCity });
     } catch {
       dispatch({
         type: "rejected",
@@ -199,32 +126,22 @@ function CitiesProvider({ children }) {
     }
   }
 
-  // async function deleteCity(id) {
-  //   try {
-  //     setIsLoading(true);
-  //     await fetch(`${BASE_URL}/cities/${id}`, {
-  //       method: "DELETE",
-  //     });
 
-  //     // console.log(data);
-
-  //     setCities((cities) => cities.filter((city) => city.id !== id));
-  //   } catch {
-  //     alert("There was an error deleting data");
-  //   } finally {
-  //     setIsLoading(false);
-  //   }
-  // }
-
+  //UPDATED FOR FIREBASE:
   async function deleteCity(id) {
     dispatch({ type: "loading" });
 
     try {
-      await fetch(`${BASE_URL}/cities/${id}`, {
+      const res = await fetch(`${BASE_URL}/cities.json`);
+      const data = await res.json();
+
+      const keyToDelete = Object.keys(data).find((key) => data[key].id === id);
+
+      if (!keyToDelete) throw new Error("City not found");
+
+      await fetch(`${BASE_URL}/cities/${keyToDelete}.json`, {
         method: "DELETE",
       });
-
-      // console.log(data);
 
       dispatch({ type: "city/deleted", payload: id });
     } catch {
